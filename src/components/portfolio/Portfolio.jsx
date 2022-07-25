0e7e48b6718f14468d0ef280675a19db0adc6ea8
Omar Salem <omarsalem0721@gmail.com>
2022-07-25 19:10:47 +0200

    Fix style for portfolio projects

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import PortfolioList from "../portfolioList/PortfolioList";
+import { GrView } from "react-icons/gr";
+import { IconContext } from "react-icons";
 import "./portfolio.scss";
 import {
   featuredPortfolio,
@@ -33,8 +35,6 @@ export default function Portfolio() {
       id: "HTML/CSS",
       title: "HTML/CSS",
     },
-  
- 
   ];
 
   useEffect(() => {
@@ -79,14 +79,49 @@ export default function Portfolio() {
       <div className="container">
         {data.map((d) => (
           <div
+            style={{
+              backgroundImage: `url(${d.img})`,
+              backgroundPosition: "center",
+              backgroundSize: "contain",
+              backgroundRepeat: "no-repeat",
+            }}
             className="item"
             key={Math.random() * 100}
-            onClick={() =>
-              window.open(d.link, "_blank")
-            }
           >
-            <img src={d.img} alt="" />
-            <h3>{d.title}</h3>
+            <div className="itemContainer">
+              <div className="itemContent">
+                <h3>{d.title}</h3>
+                <div className="icons">
+                  {true ? (
+                    ""
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="20"
+                      height="20"
+                      fill="currentColor"
+                      class="bi bi-eye"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
+                      <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
+                    </svg>
+                  )}
+                  <svg
+                    onClick={() => window.open(d.link, "_blank")}
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    fill="currentColor"
+                    class="bi bi-github"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z" />
+                  </svg>
+                </div>
+                {/* <GrView style={{ color: "white", fontSize: "1.5em" }} /> */}
+              </div>
+            </div>
           </div>
         ))}
       </div>
