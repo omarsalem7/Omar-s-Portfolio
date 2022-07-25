fec6fea5001ff7bf6af5d1e307ba9f18a0fcbc57
Omar Salem <omarsalem0721@gmail.com>
2022-07-25 20:03:23 +0200

    Add social contacts

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,8 @@
 import "./topbar.scss";
 import { BsFillPersonFill } from "react-icons/bs";
 import { GrMail } from "react-icons/gr";
+import { FaLinkedin } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
 export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
     <div className={"topbar " + (menuOpen && "active")}>
@@ -14,8 +16,27 @@ export default function Topbar({ menuOpen, setMenuOpen }) {
             <span>+201099348209</span>
           </div>
           <div className="itemContainer">
-            <GrMail className="icon" />
-            <span>omarsalem0721@gmail.com</span>
+            <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=omarsalem0721@gmail.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <GrMail className="icon" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/omarsalem7/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FaLinkedin className="icon" />
+            </a>
+            <a
+              href="https://github.com/omarsalem7"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <AiFillGithub className="icon" />
+            </a>
           </div>
         </div>
         <div className="right">
